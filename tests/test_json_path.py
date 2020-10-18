6c98d9e2232ba0b9722b26eaaa0a985d73e6c8f6
Susannah Klaneček <suzil@protonmail.ch>
2020-10-18 12:42:28 -0400

    Add test for valid reference paths
    Found examples here: https://states-language.net/spec.html#ref-paths
    
    Additionally added a defensive assert that there should only be one match when applying a reference path.

--- tests/test_json_path.py
+++ tests/test_json_path.py
@@ -2,7 +2,7 @@ import re
 
 import pytest
 
-from awsstepfuncs.json_path import apply_json_path
+from awsstepfuncs.json_path import apply_json_path, validate_json_path
 
 
 @pytest.fixture(scope="session")
@@ -36,3 +36,28 @@ def test_apply_json_path_must_begin_with_dollar(sample_data):
 
 def test_apply_json_path_no_match(sample_data):
     assert apply_json_path("$.notfound", sample_data) is None
+
+
+@pytest.mark.parametrize(
+    "reference_path",
+    [
+        r"$.store.book",
+        r"$.store\.book",
+        r"$.\stor\e.boo\k",
+        r"$.store.book.title",
+        r"$.foo.\.bar",
+        # TODO: The following reference path should be valid by example: https://states-language.net/spec.html#ref-paths
+        # But there is a "?" which is considered an invalid operator, but perhaps in
+        # this scenario it's not considered an operator
+        # "$.foo\@bar.baz\[\[.\?pretty",
+        r"$.&Ж中.\uD800\uDF46",
+        r"$.ledgers.branch[0].pending.count",
+        r"$.ledgers.branch[0]",
+        r"$.ledgers[0][22][315].foo",
+        r"$['store']['book']",
+        r"$['store'][0]['book']",
+    ],
+)
+def test_valid_reference_path(reference_path):
+    # Should raise no ValueError
+    validate_json_path(reference_path)
