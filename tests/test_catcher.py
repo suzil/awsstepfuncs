5f5f6740a4919610169991a26bb8b97687143fb5
Susannah Klaneček <suzil@users.noreply.github.com>
2020-10-23 12:25:09 -0500

    Add support for States.ALL catcher (#46)

---
+++ tests/test_catcher.py
@@ -0,0 +1,44 @@
+import contextlib
+from contextlib import redirect_stdout
+from io import StringIO
+
+from awsstepfuncs import FailState, SucceedState, TaskState
+from awsstepfuncs.state_machine import StateMachine
+
+
+def test_catcher():
+    resource = "123"
+    task_state = TaskState("Task", resource=resource)
+    succeed_state = SucceedState("Success")
+    fail_state = FailState("Failure", error="IFailed", cause="I failed!")
+
+    task_state >> succeed_state
+    task_state.add_catcher(["States.ALL"], next_state=fail_state)
+
+    state_machine = StateMachine(start_state=task_state)
+
+    def success_mock_fn(_):
+        assert True
+
+    def failure_mock_fn(_):
+        assert False  # noqa: PT015
+
+    stdouts = {}
+    for mock_fn in [success_mock_fn, failure_mock_fn]:
+        with contextlib.closing(StringIO()) as fp:
+            with redirect_stdout(fp):
+                state_machine.simulate(resource_to_mock_fn={resource: mock_fn})
+            stdouts[mock_fn] = fp.getvalue()
+
+    assert (
+        stdouts[success_mock_fn]
+        == """Running Task
+Running Success
+"""
+    )
+    assert (
+        stdouts[failure_mock_fn]
+        == """Running Task
+Running Failure
+"""
+    )
