d8f30f340794372923bf740433f5ac144ef702d9
Susannah Klaneček <suzil@protonmail.ch>
2020-11-03 10:17:43 -0600

    Split condition and choices to a separate module choice.py

--- src/awsstepfuncs/__init__.py
+++ src/awsstepfuncs/__init__.py
@@ -2,17 +2,19 @@
 
 # Version based on .git/refs/tags - make a tag/release locally, or on GitHub (and pull)
 from awsstepfuncs._repo_version import version as __version__  # noqa:F401
-from awsstepfuncs.state import (  # noqa: F401
+from awsstepfuncs.choice import (  # noqa: F401
     AndChoice,
-    ChoiceState,
     Condition,
+    NotChoice,
+    VariableChoice,
+)
+from awsstepfuncs.state import (  # noqa: F401
+    ChoiceState,
     FailState,
     MapState,
-    NotChoice,
     PassState,
     SucceedState,
     TaskState,
-    VariableChoice,
     WaitState,
 )
 from awsstepfuncs.state_machine import StateMachine  # noqa: F401
