adfb5a9b805cccae5dc08e4dc68b3519774a414f
Susannah Klaneček <suzil@protonmail.ch>
2020-11-02 20:17:37 -0600

    Move input and output path tests to doctests

--- tests/test_pass_state.py
+++ tests/test_pass_state.py
@@ -76,44 +76,6 @@ def test_pass_state(compile_state_machine):
     ]
 
 
-def test_input_path_output_path(compile_state_machine):
-    input_path = "$.dataset2"
-    output_path = "$.val1"
-    pass_state = PassState("Pass 1", input_path=input_path, output_path=output_path)
-
-    # Define the state machine
-    state_machine = StateMachine(start_state=pass_state)
-
-    # Check the output from compiling
-    compiled = compile_state_machine(state_machine)
-    assert compiled == {
-        "StartAt": pass_state.name,
-        "States": {
-            pass_state.name: {
-                "InputPath": input_path,
-                "OutputPath": output_path,
-                "Type": "Pass",
-                "End": True,
-            },
-        },
-    }
-
-    # Simulate the state machine
-    state_input = {
-        "comment": "Example for InputPath.",
-        "dataset1": {"val1": 1, "val2": 2, "val3": 3},
-        "dataset2": {"val1": "a", "val2": "b", "val3": "c"},
-    }
-    state_output = state_machine.simulate(state_input=state_input)
-    assert state_output == state_input["dataset2"]["val1"]
-
-
-def test_state_has_invalid_input_path():
-    invalid_input_path = "$.dataset*"
-    with pytest.raises(ValueError, match='Unsupported ReferencePath operator: "*"'):
-        PassState("Pass 1", input_path=invalid_input_path)
-
-
 def test_state_name_too_long():
     with pytest.raises(
         ValueError, match=r'State name "[a]+" must be less than 128 characters'
